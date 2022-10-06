6e0e6db3238819e30267e3e998f11a6489473e1f
ntem1996 <57796331+ntem1996@users.noreply.github.com>
2022-10-06 13:59:50 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -19,3 +19,17 @@ function menuSluiten () {
     var deNav = document.querySelector("header nav:nth-of-type(2)");
     deNav.classList.remove("open");
 }
+
+// knop voor filteren openen 
+
+var filterOpenKnop = document.querySelector("main section:nth-of-type(2)");
+
+filterOpenKnop.addEventListener("click", filterOpenen);
+
+function filterOpenen () {
+    var filter = document.querySelector("main section:nth-of-type(2)");
+    filter.classList.toggle("filterOpen");
+}
+
+
+
